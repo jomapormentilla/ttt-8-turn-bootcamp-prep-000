a3734f9e4687d1937bbce129e232b0b21528e37f
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-29 16:24:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,10 @@ def display_board(board)
   puts " #{board[0]} | #{board[0]} | #{board[0]} "
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+end
+
 def valid_move?(board, index)
   if( index.between(0, 8) && !position_taken?(board, index) )
     return true
