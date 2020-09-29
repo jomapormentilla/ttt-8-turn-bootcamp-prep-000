74ec2ba601f8ac0c00fa82eb1dd8ee550320809d
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-29 16:23:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,28 @@
-def display_board
-  
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
 end
 
-def valid_move
-  
+def valid_move?(board, index)
+  if( index.between(0, 8) && !position_taken?(board, index) )
+    return true
+  else
+    return false
+  end
 end
 
-def move
-  
+def move(board, input, player="X")
+  index = input.to_i - 1
+  board[index] = player
 end
 
 def position_taken?(board, index)
-  
+  if(board[index] == "" || board[index] == " " || board[index] == nil)
+    return false
+  else
+    return true
+  end
 end
