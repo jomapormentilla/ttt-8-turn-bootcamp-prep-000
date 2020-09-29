d72c6a8121933e6f57dcb9426c8aab34762ede66
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-29 16:38:49 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ def turn(board)
 end
 
 def valid_move?(board, index)
-  if( index.between(0, 8) && !position_taken?(board, index) )
+  if( index.between?(0, 8) && !position_taken?(board, index) )
     return true
   else
     return false
